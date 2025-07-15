8291e69e1888af1d92854c96a4dcb9b480ac9098
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-07-15 23:37:49 +0200

    Time: 0 ms (100.00%) | Memory: 23.4 MB (40.88%) - LeetSync

--- 338-counting-bits/counting-bits.swift
+++ 338-counting-bits/counting-bits.swift
@@ -1,20 +1,7 @@
 class Solution {
     func countBits(_ n: Int) -> [Int] {
 
-        var ans: [Int] = []
+        return (0...n).map{$0.nonzeroBitCount}
 
-        for i in 0...n{
-            let binStr = String(i, radix: 2)
-            //if let num = Int(binStr) { ans.append(num) } else { }
-            let sumStr = binStr.compactMap(\.wholeNumberValue).reduce(0, +)
-
-            let num = Int(sumStr) ?? 0
-
-            ans.append(num)
-        }
-        
-
-        return ans
-        
     }
 }

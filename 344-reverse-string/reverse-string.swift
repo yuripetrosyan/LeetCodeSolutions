7968d1cbc4dbe6bdbad840135d9fe21ea8114e9e
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-07-15 23:52:44 +0200

    Time: 0 ms (100.00%) | Memory: 21.8 MB (81.75%) - LeetSync

--- 344-reverse-string/reverse-string.swift
+++ 344-reverse-string/reverse-string.swift
@@ -1,6 +1,14 @@
 class Solution {
     func reverseString(_ s: inout [Character]) {
+        
+        var left = 0
+        var right = s.count - 1
+        //return s.reverse()
 
-        return s.reverse()
+        while left < right {
+            s.swapAt(left, right)
+            left += 1
+            right -= 1
+        }
     }
 }

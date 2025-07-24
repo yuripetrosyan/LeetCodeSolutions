9fcd776c10e0778587e15f01e1b88af5532d166c
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-07-24 21:17:10 +0200

    Time: 19 ms (35.29%) | Memory: 17.9 MB (76.47%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.swift
+++ 1580-shuffle-the-array/shuffle-the-array.swift
@@ -1,13 +1,18 @@
 class Solution {
     func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
 
-        var finalArray: [Int] = []
+          var result:[Int] = []
 
-        for i in 0..<n{
-            finalArray.append(nums[i])
-            finalArray.append(nums[n + i])
-        }
 
-        return finalArray
+          for i in 0..<n{
+            result.append(nums[i])
+            result.append(nums[n + i])
+
+          }
+
+
+
+          return result  
+
     }
 }

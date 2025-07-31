f9ad062ed46f805554d95f1556d120e7949a4231
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-07-31 18:01:12 +0200

    Time: 0 ms (100.00%) | Memory: 19.3 MB (85.71%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.swift
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.swift
@@ -1,19 +1,13 @@
 class Solution {
     func prefixCount(_ words: [String], _ pref: String) -> Int {
-
-        var arr:[String] = []
-        let n:Int = Array(pref).count
-        var count = 0
-        
+     
+        var count = 0     
         for i in words{
-            arr.append(String(i.prefix(min(n, i.count))))
+           
+           if i.hasPrefix(pref){
+            count += 1
+           }
             
-        }
-
-        for j in arr {
-            if j.contains(pref){
-                count += 1
-            }
         }
 
         return count

28815e53d1cabe48554464a52e10e61e42821aec
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-08-01 12:07:05 +0200

    Time: 109 ms (20.00%) | Memory: 20 MB (46.67%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func firstPalindrome(_ words: [String]) -> String {
+       
+      for word in words{
+        if word == String(word.reversed()) {
+            return word
+        }
+      }
+      return ""
+        
+    }
+    }

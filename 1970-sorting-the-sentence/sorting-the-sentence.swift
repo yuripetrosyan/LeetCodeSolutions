e313c25bff57ee04bbbd834da29f9936135896cc
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-08-03 14:35:19 +0200

    Time: 5 ms (40.00%) | Memory: 19.2 MB (53.33%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.swift
+++ 1970-sorting-the-sentence/sorting-the-sentence.swift
@@ -1,27 +1,11 @@
 class Solution {
     func sortSentence(_ s: String) -> String {
         
-        var arr = s.components(separatedBy: " ")
-        var res: [String] = []
-        var fin: [String] = []
-
-        for str in arr{
-
-         let lastChar = str.last! // Get last character as Character
-         let startIndex = str.startIndex
-         let endIndex = str.index(before: str.endIndex) // One before the end
-         let rest = str[startIndex..<endIndex] // Everything except last char
-    
-          res.append(String(lastChar) + rest)
-
-        }
-
-        res = res.sorted()
-
-        for i in res{
-            fin.append(String(i.dropFirst()))
-        }
-
-        return fin.joined(separator: " ")
+        s
+        .split(separator: " ")
+        .sorted { $0.last! < $1.last! }
+        .map {String($0.dropLast())}
+        .joined(separator: " ")
+       
     }
 }

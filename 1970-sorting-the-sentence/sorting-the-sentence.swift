6cab043ca17a7c42ef8a66e03a8717747a07ef0b
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-08-03 14:42:25 +0200

    Time: 7 ms (20.00%) | Memory: 18.6 MB (80.00%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.swift
+++ 1970-sorting-the-sentence/sorting-the-sentence.swift
@@ -1,11 +1,10 @@
 class Solution {
     func sortSentence(_ s: String) -> String {
-        
         s
         .split(separator: " ")
         .sorted { $0.last! < $1.last! }
-        .map {String($0.dropLast())}
+        .map{$0.dropLast()}
         .joined(separator: " ")
-       
+     
     }
 }

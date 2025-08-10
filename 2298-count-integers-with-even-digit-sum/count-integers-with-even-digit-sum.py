7e7ae5fd9d2036674650669d75e600017f43fc76
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-08-10 18:30:24 +0200

    Time: 11 ms (48.36%) | Memory: 17.7 MB (89.18%) - LeetSync

--- 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.py
+++ 2298-count-integers-with-even-digit-sum/count-integers-with-even-digit-sum.py
@@ -1,10 +1,4 @@
 class Solution:
     def countEven(self, num: int) -> int:
 
-        res = 0
-
-        for i in range(1, num + 1):
-           if sum(map(int, str(i))) % 2 == 0: 
-            res += 1
-    
-        return res
+        return sum(sum(map(int, str(i))) % 2 == 0 for i in range(1, num + 1))

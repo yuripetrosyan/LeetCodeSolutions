f165df4c3e422c65c23569eb6b86209823fc9eae
Yuri Petrosyan <73176415+yuripetrosyan@users.noreply.github.com>
2025-08-10 15:12:37 +0200

    Time: 2 ms (64.99%) | Memory: 18.4 MB (78.15%) - LeetSync

--- 3188-find-champion-i/find-champion-i.py
+++ 3188-find-champion-i/find-champion-i.py
@@ -2,15 +2,10 @@ class Solution:
     def findChampion(self, grid: List[List[int]]) -> int:
 
         arr = []
-        res = 0
-      
 
         for i in grid:
             arr.append(sum(i))
 
-        for i in range(len(arr)):
-            res = arr.index(max(arr))
-
-        return res
+        return arr.index(max(arr))
             
         
